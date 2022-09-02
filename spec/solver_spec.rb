4f61ca8da099d0dd4b2f93b055a107467137434c
unknown <hayounn.etd@gmail.com>
2022-09-02 15:28:08 +0100

    Write tests for the everse method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -27,4 +27,23 @@ describe 'Solver' do
             expect(@solver.factorial(number)).to eq("Please enter a positive integer")
         end
     end
+
+    describe '#reverse' do
+        it "If string is hello, return olleh" do
+            string = "hello"
+            expect(@solver.reverse(string)).to eq('olleh')
+        end
+
+        it "If string is morning, return gninrom" do
+            string = "hello"
+            expect(@solver.reverse(string)).to eq('gninrom')
+        end
+
+        it "If string is empty reutn nil" do
+            string = ""
+            expext(@solver.reverse(string)).to eq("")
+        end
+
+    end
+
 end
